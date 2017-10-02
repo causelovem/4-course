07d013abd8d1e216dad15af10979e7e31c7b436d
Goalt <yurikonkov@MacBook-Pro-Yuri.local>
2017-10-02 15:34:24 +0300

    added solutions and folder was renamed

---
+++ UNИX Python 2017/task2.py
@@ -0,0 +1,7 @@
+#AndOr
+a = eval(input())
+b = eval(input())
+c = a or b
+if (not c):
+	c = "NO"
+print(c)
---
+++ UNИX Python 2017/task4.py
@@ -0,0 +1,20 @@
+# IntPalindrome
+a = eval(input())
+c = int(1)
+while c < a:
+    c *= 10
+c = int(c / 10)
+result = 'YES'
+
+while a > 10:
+    first = int(a / c)
+    last = a % 10
+    a -= first * c
+    a = int(a / 10)
+    c = int(c / 100)
+    #print(first, ' ', last)
+    if (first != last):
+        result = 'NO'
+        break
+
+print(result)
