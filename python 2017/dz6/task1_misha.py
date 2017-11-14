7ac4d69c3e97742c207fe8bd1e1947b33eb57edc
causelovem <rat.taurus@gmail.com>
2017-11-14 21:15:04 +0300

    task1

--- python 2017/dz6/task1_misha.py
+++ python 2017/dz6/task1_misha.py
@@ -6,14 +6,16 @@ inp = set(eval(input()))
 
 m = max(inp)
 s = set()
-le = len(set(inp))
 
-M = math.trunc(math.sqrt(m))
-for i in range(1, M + 1):
-    Mi = math.trunc(math.sqrt(m - i ** 2))
-    for j in range(i, Mi + 1):
-        Mij = math.trunc(math.sqrt(m - i ** 2 - j ** 2))
+M = int(math.sqrt(m))
+
+for i in range(1, int((M + 1))):
+    Mi = int(math.sqrt((m - i ** 2)))
+    ii = i ** 2
+    for j in range(i, int((Mi + 1))):
+        jj = j ** 2
+        Mij = int(math.sqrt(m - ii - jj))
         for k in range(j, Mij + 1):
-            s.add((i ** 2 + j ** 2 + k ** 2))
+            s.add((ii + jj + k ** 2))
 
 print(len(set(inp) & s))
